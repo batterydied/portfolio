04c2150d8680e560f209d8083a6e2b25f6f4f92f
Benson Zheng <bensonzheng2003@gmail.com>
2026-01-14 04:21:30 -0500

    removed pointer from hovering over name

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     return (
         <div className="w-full px-2 py-6 flex-rol items-center">
             <div className="w-full flex justify-between items-center">
-                <div className="text-[var(--text)] font-bold text-2xl hover:cursor-pointer">
+                <div className="text-[var(--text)] font-bold text-2xl">
                 BENSON ZHENG
                 </div>
                 <div className="text-[var(--border)] flex items-center justify-center gap-2" >
